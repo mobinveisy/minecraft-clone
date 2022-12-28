a2a39d2275c595074d3325baeabd1200cab2dec6
Mobin Veisy <ebo952@gmail.com>
2022-12-28 09:58:17 +0330

    starter codes

---
+++ Steps.md
@@ -0,0 +1,18 @@
+- [x] Boilerplate
+- [x] Sky
+- [x] Textures and images
+- [x] Ground
+- [] keyboard inputs
+- [] Player
+- [] First person view
+- [] Gravity
+- [] Movement
+- [] State management
+- [] Cubes
+- [] Cube type selector
+- [] Save world in localstorage
+- [] Build a house
+
+
+
+⌚0:25:29
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Physics } from '@react-three/cannon';
+import { Sky } from '@react-three/drei';
+import  {Ground}  from './Ground';
+import { Canvas } from '@react-three/fiber';
+import { Player } from './Player';
+
+function App() {
+  return (
+    <>
+      <Canvas>
+        <Sky sunPosition={[100,100,20]}/>
+        <ambientLight intensity={0.5}/>
+        <Physics>
+          <Player/>
+          <Ground/>
+        </Physics>
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
